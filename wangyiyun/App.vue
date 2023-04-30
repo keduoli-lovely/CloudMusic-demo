cd58d4c6a03b8f1ead52f36ad942fff3db68061d
keduoli-lovely <95008862+keduoli-lovely@users.noreply.github.com>
2023-04-30 18:09:54 +0800

    Update App.vue

--- wangyiyun/App.vue
+++ wangyiyun/App.vue
@@ -1,19 +1,3 @@
-<script>
-	
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-			
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
-</script>
-
 <style>
 	.daytime {
 		
