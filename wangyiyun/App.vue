a19eac839f3d071c95646d6e537e1be8440112ab
keduoli-lovely <95008862+keduoli-lovely@users.noreply.github.com>
2023-05-02 16:19:49 +0800

    不再更新该分支

--- wangyiyun/App.vue
+++ wangyiyun/App.vue
@@ -1,36 +1,38 @@
+
+
+
 <style>
-	.daytime {
-		
-	}
+	.daytime {}
+
 	.night {
 		/* --------- 首页-------- */
 		/* // 背景 */
 		--indexgbcolor: #151515;
 		--indexiconbgcolor: #2c2c2c;
 		--indexsearchbgcolor: #2f2f2d;
-		
+
 		/* // 字体 */
 		--indexfontcolor: #fff;
 		--zhuanshufontcolor: #fff;
 		--liveboxfontcolor: #fefefe;
-		
-		
+
+
 		/* // 边框颜色 */
 		--bordercolor: #4a4a4a;
-		
+
 		/* // 加载更多按钮相关 */
 		--btnbgcolor: #d0d0d0;
 		--btnfontcolor: #7d7d7d;
-		
+
 		/* --------- 首页-------- */
-		
-		
+
+
 		/* --------- 搜索页-------- */
 		--topsearchbgcolor: #2c2c2c;
-		
-		
-		
-		
+
+
+
+
 		/* --------- 搜索页-------- */
 		/* --------- 搜索列表页-------- */
 		--searchlistbgcolor: #141414;
@@ -43,29 +45,32 @@
 		--listboxtextcolor: #567d9e;
 		--listboxrowcolor: #cac6c7;
 		/* --------- 搜索列表页-------- */
-		
-		
-		
-		
+
+
+
+
 		/* --------- 历史播放列表页-------- */
 		--historybgcolor: #151515;
-		
+
 		/* --------- 历史播放列表页-------- */
 	}
-	::-webkit-scrollbar{
-	    display: none;
+
+	::-webkit-scrollbar {
+		display: none;
 	}
+
 	image {
 		width: 100%;
 		height: 100%;
 	}
+
 	.p {
 		display: flex;
 		/* flex-direction: column; */
 		justify-content: center;
 		align-items: center;
 		flex-wrap: wrap;
-		color: rgba(255,255,255,.6);
+		color: rgba(255, 255, 255, .6);
 		font-size: 14px;
 		height: 60rpx;
 		line-height: 40rpx;
