80836df3224a6be17ae994d0fd0169ad60b0e040
keduoli-lovely <95008862+keduoli-lovely@users.noreply.github.com>
2023-05-12 23:03:22 +0800

    关注页面

---
+++ wangyiyun重写版/pages/concern/concern.vue
@@ -0,0 +1,548 @@
+<template>
+	<view class="concern night">
+		<view class="mask-essay" v-if="showmore" @click="sendcon">
+
+		</view>
+		<view class="header">
+			<indexleftbar></indexleftbar>
+
+			<view class="header-title">
+				动态
+			</view>
+
+			<view class="addessay">
+				<view class="essayicon" @click="sendcon">
+					<image src="/static/icon/add.png" mode=""></image>
+				</view>
+
+				<view class="more" v-if="showmore">
+					<view class="mask">
+
+					</view>
+					<view class="sendtext">
+						发图文
+					</view>
+					<view class="sendvideo">
+						发视频
+					</view>
+					<view class="ksong">
+						k歌
+					</view>
+					<view class="recording">
+						录声音
+					</view>
+				</view>
+			</view>
+		</view>
+
+		<view class="owned">
+			<view class="myowned">
+				<view class="owed-img" style="background-color: #D9AFD9;
+background-image: linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);padding: 15rpx;
+">
+					<image src="/static/icon/全球.png" mode="aspectFill"></image>
+				</view>
+				<text class="t1">我的圈子</text>
+			</view>
+
+			<view class="scrollowned">
+
+				<view class="myowned" v-for="(item, index) in follow" :key="index">
+					<view class="owed-img">
+						<image :src="item.avatarUrl" mode="aspectFill"></image>
+					</view>
+					<text class="t1">{{item.nickname}}</text>
+				</view>
+
+				<view class="myowned">
+					<view class="owed-img" style="background-color: #666;padding: 14rpx">
+						<image src="/static/icon/更多2.png" mode="aspectFill"></image>
+					</view>
+					<text class="t1">全部好友</text>
+				</view>
+			</view>
+
+		</view>
+
+
+		<view class="choose">
+			<view class="line">
+				<!-- // 线条 -->
+			</view>
+			<view class="choosebar">
+				<view class="bar-left">
+					我的关注
+				</view>
+
+				<view class="bar-right">
+					<view class="all">
+						全部
+					</view>
+
+					<view class="song">
+						音乐人
+					</view>
+
+					<view class="firend">
+						朋友
+					</view>
+				</view>
+			</view>
+
+			<view class="con">
+
+			</view>
+		</view>
+
+		<NewsThatCares 
+		v-for="(item, index) in liveusertolive"
+		:name="item.user.nickname"
+		:pic="item.user.avatarUrl"
+		:id="item.user.userId"
+		:con="item.json"
+		:livenum="item.info.likedCount"
+		:plun="item.info.commentCount"
+		:give="item.insiteForwardCount"
+		></NewsThatCares>
+		
+		
+		<minmusic
+		v-show="$store.state.showplaycomponent"
+		:imgk="musicpic"
+		:name="name" 
+		:ren="ren" 
+		:dd="dd"
+		:love="love"
+		:indexk="isindex"
+		:top="topnum"
+		:isshow="isshow"
+		@stopkk="bilibili"
+		></minmusic>
+	</view>
+</template>
+
+<script>
+	import helper from '../../common/helper.js'
+	export default {
+		data() {
+			return {
+				lasttime: '',
+				isindex: 1,
+				isshow: 1,
+				dd: -95,
+				numk: 1,
+				showmore: false,
+				follow: [],
+				liveusertolive: [],
+				musicpic: helper.contminlist.musicpic,
+				name: helper.contminlist.name,
+				ren: helper.contminlist.ren,
+				topnum: helper.contminlist.topnum,
+				love: helper.contminlist.islove,
+			};
+		},
+		onLoad() {
+			this.getliveuser()
+		},
+		onReachBottom() {
+			this.getlasttime()
+			uni.showToast({
+				title: '加载中...',
+				duration: 1500,
+				mask:true,
+				icon: 'none'
+			});
+		},
+		mounted() {
+			uni.$on('conceplay', this.playmin)
+		},
+		methods: {
+			bilibili(e) {
+				if(this.numk != e) {
+					this.numk = 1
+					uni.showTabBar({
+						fail() {
+							console.log('珂朵莉')
+						}
+					})
+					helper.contminlist.dd = -100
+					this.dd = -95
+					// console.log(11,'k')
+				} else {
+					this.dd = -100
+					this.numk = e
+					helper.contminlist.dd = 0
+				}
+			},
+				playmin(e) {
+					this.startgq(e.id, e.img, e.title, e.name, e.size)
+				},
+				stormusiclist(e, img, title, name, size, love) {
+					let value = uni.getStorageSync('musiclist');
+					let np = false
+					if(value) {
+						let kenum = value.findIndex(function(item) {
+							return item.e === e
+						})
+						if(kenum >= 0) {
+							this.$store.commit('changeSubscript', kenum)
+						}else if(kenum <= 0) {
+							this.$store.commit('changeSubscript', 0)
+						}
+					}
+						
+					if(value.length > 0) {
+						let keke = value.filter((item) => {
+							return item.e == e
+						})
+						if(keke.length == 0) {
+							np = true
+						}else {
+							np = false
+						}
+					}else if(!value || value.length === 0) {
+						uni.setStorageSync('musiclist', [{
+							e,
+							title,
+							name,
+							img,
+							size,
+							love
+						}])
+					
+					}
+					
+					if(np) {
+						let tp = value
+						tp.unshift({
+							e,
+							title,
+							name,
+							img,
+							size,
+							love
+						})
+						uni.setStorageSync('musiclist', tp);
+					}
+				},
+				islove(id) {
+					let idlist = uni.getStorageSync('userliveid')
+					let x
+					if(idlist) {
+						x = idlist.some(item => {
+							return item === id
+						})					
+					}
+					return x
+				},
+				startgq(id,img, title, name,playsize) {
+					uni.hideTabBar()
+					uni.request({
+						url: `${helper.url}/check/music?id=${id}`,
+						success: (res) => {
+							this.isplay = res.data.success
+					
+					
+							if(!this.isplay) {
+								uni.showToast({
+									title: res.data.message,
+									duration: 1000,
+									mask: true,
+									icon: 'none'
+								});
+								return
+							}
+							getApp().watchmusice()
+							
+							this.love = this.islove(id)
+							helper.contminlist.islove = this.love
+							helper.audiok.onEnded(() => {
+								if (uni.getStorageSync('musiclist').length <= 1) {
+									helper.audiok.src = ''
+									this.$store.commit('changeControl', 1)
+								} else {
+									if(this.$refs.child != undefined) {
+										this.$refs.child.down()
+									}else {
+										uni.$emit('playlistdown')
+									}
+								}
+							})
+							this.id = id
+							helper.moredetail.push({
+								"name": title,
+								"singer": name
+							})
+							this.hello = 0
+							
+							this.$store.commit('changeControl', 0)
+							uni.request({
+								url: `${helper.url}/comment/music?id=${id}&limit=1`,
+								success: (res) => {
+									let sizek = res.data.total
+									if (sizek < 10000) {
+										this.topnum = '999+'
+										helper.plnumstr = '999+'
+									} else if (sizek >= 10000 && sizek < 100000) {
+										this.topnum = '1w+'
+										helper.plnumstr = '1w+'
+									} else if (sizek > 100000) {
+										
+										this.topnum = '10w+'
+										helper.plnumstr = '10w+'
+									}
+									this.stormusiclist(id, img, title, name, this.topnum, this.love)
+								}
+							})
+						
+							uni.request({
+								url: `${helper.url}/song/url/v1?id=${id}&level=exhigh`,
+								success: (res) => {
+						
+									this.musicpic = img
+									helper.contminlist.musicpic = img
+									
+									this.name = title
+									helper.contminlist.name = title
+									
+									this.ren = name
+									helper.contminlist.ren = name
+									
+									
+									this.dd = 0
+									helper.contminlist.dd = 0
+									
+									this.$store.commit('changeshow', 1)
+									
+									helper.audiok.autoplay = true
+									helper.audiok.src = res.data.data[0].url
+				
+								}
+							})
+						}	
+				  })
+				},
+			getlasttime() {
+				let id = uni.getStorageSync('id')
+				if(!id && !this.follow) return
+				uni.request({
+					url: `${helper.url}/user/follows?uid=${id}`,
+					success: (res) => {
+						this.follow = res.data.follow
+						
+						if(this.follow) {
+							this.follow.forEach(item => {
+								uni.request({
+									url: `${helper.url}/user/event?uid=${item.userId}&lasttime=${this.lasttime}`,
+									success: (res) => {
+										console.log(res)
+										this.lasttime = res.data.lasttime
+										let rows = res.data.events
+										this.liveusertolive = [...rows, ...this.liveusertolive]
+									}
+								})
+							})
+						}
+					}
+				})
+			},
+			getliveuser() {
+				let id = uni.getStorageSync('id')
+				if(!id && !this.follow) return
+				uni.request({
+					url: `${helper.url}/user/follows?uid=${id}`,
+					success: (res) => {
+						this.follow = res.data.follow
+						
+						if(this.follow) {
+							this.follow.forEach(item => {
+								uni.request({
+									url: `${helper.url}/user/event?uid=${item.userId}`,
+									success: (res) => {
+										if(this.lasttime == res.data.lasttime) {
+											uni.showToast({
+												title: '没有更多了',
+												duration: 1500,
+												mask:true,
+												icon: 'none'
+											});
+											return
+											
+										}
+										this.lasttime = res.data.lasttime
+										let rows = res.data.events
+										this.liveusertolive = [...rows, ...this.liveusertolive]
+									}
+								})
+							})
+						}
+					}
+				})
+			},
+			sendcon() {
+				this.showmore = !this.showmore
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	* {
+		box-sizing: border-box;
+	}
+
+	.concern {
+		padding: 140rpx 20rpx 140rpx;
+		width: 750rpx;
+		// height: 100v;
+		overflow: hidden;
+		background-color: var(--searchlistbgcolor);
+		.mask-essay {
+			z-index: 999;
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+
+		.header {
+			z-index: 99;
+			display: flex;
+			justify-content: space-between;
+			padding: 40rpx 40rpx 40rpx 30rpx;
+			position: fixed;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 140rpx;
+			// background-color: #999;
+			background-color: var(--searchlistbgcolor);
+			.header-title {
+				font-size: 34rpx;
+				color: var(--indexfontcolor);
+			}
+			.addessay {
+				position: relative;
+				height: 60rpx;
+
+				.essayicon {
+					width: 50rpx;
+					height: 50rpx;
+					overflow: hidden;
+					border-radius: 50%;
+					background-color: red;
+					border: 8rpx solid red;
+				}
+
+				.more {
+					position: absolute;
+					right: -28rpx;
+					top: 80rpx;
+					width: 280rpx;
+					padding: 0 40rpx;
+					background-color: skyblue;
+					border-radius: 14rpx;
+
+					.mask {
+						position: relative;
+						width: 100%;
+						height: 1rpx;
+					}
+
+					.mask::before {
+						content: '';
+						position: absolute;
+						top: -16rpx;
+						left: 170rpx;
+						width: 0;
+						height: 0;
+						border: 38rpx;
+						border-bottom: 20rpx solid skyblue;
+						border-right: 20rpx solid transparent;
+						border-left: 20rpx solid transparent;
+					}
+
+					.sendtext,
+					.sendvideo,
+					.ksong,
+					.recording {
+						padding: 30rpx;
+						border-bottom: 1rpx solid #e0e0e0;
+					}
+
+					.recording {
+						border: 0;
+					}
+				}
+			}
+		}
+
+		.owned {
+			display: flex;
+			align-items: center;
+			margin-bottom: 38rpx;
+
+			.myowned {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				padding-right: 30rpx;
+
+				.owed-img {
+					margin-bottom: 8rpx;
+					width: 120rpx;
+					height: 120rpx;
+					overflow: hidden;
+					border-radius: 50%;
+					background-color: #fff;
+				}
+				.t1 {
+					color: var(--indexfontcolor);
+					font-size: 28rpx;
+				}
+			}
+
+			.scrollowned {
+				overflow-x: auto;
+				display: flex;
+			}
+		}
+
+		.choose {
+			position: relative;
+			padding-top: 10rpx;
+
+			.line {
+				position: absolute;
+				top: 0;
+				left: -30rpx;
+				width: 150%;
+				height: 1rpx;
+				background-color: var(--bordercolor);
+			}
+
+			.choosebar {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+
+				.bar-left {
+					flex: 4;
+					color: var(--indexfontcolor);
+				}
+
+				.bar-right {
+					color: var(--btnfontcolor);
+					flex: 3;
+					display: flex;
+					justify-content: space-between;
+					.all {
+						color: var(--indexfontcolor);
+					}
+				}
+			}
+		}
+	}
+</style>
