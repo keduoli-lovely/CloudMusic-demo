f971584c5d614caa8b00a65b09ea9ffb55f635cd
keduoli-lovely <95008862+keduoli-lovely@users.noreply.github.com>
2023-05-13 20:08:46 +0800

    动态没有消息则显示该组件

---
+++ wangyiyun重写版/components/spareWheel/spareWheel.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="sparewheel">
+		<view class="bgicon">
+			<image src="/static/icon/06-还没有数据.png" mode="aspectFill"></image>
+		</view>
+		
+		<view class="msg">
+			你关注的人三十天内没有新的消息
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"spareWheel",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.sparewheel {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding-top: 30rpx;
+		// padding: 100rpx 50rpx;
+		.bgicon {
+			width: 300rpx;
+			height: 260rpx;
+		}
+		.msg {
+			margin-top: 10rpx;
+			color: #fff;
+		}
+	}
+</style>
